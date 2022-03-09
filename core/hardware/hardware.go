52708bb88be79c88b04ea79537a9484edbaa5fd9
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-03-09 12:55:20 +0100

    add missing `JSON` annotations

--- core/hardware/hardware.go
+++ core/hardware/hardware.go
@@ -15,14 +15,14 @@ type HardwareConfig struct {
 }
 
 type PowerJob struct {
-	Id         int64
-	SwitchName string
-	TurnOn     bool
+	Id         int64  `json:"id"`
+	SwitchName string `json:"switchName"`
+	Power      bool   `json:"power"`
 }
 
 type JobResult struct {
-	Id    int64
-	Error error
+	Id    int64 `json:"id"`
+	Error error `json:"error"`
 }
 
 var log *logrus.Logger
