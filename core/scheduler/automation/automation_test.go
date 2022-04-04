565b5ad7f84023833f3cfe4adbacbad58e5f5a16
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-04 23:36:19 +0200

    Update automation_test.go

--- core/scheduler/automation/automation_test.go
+++ core/scheduler/automation/automation_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/MikMuellerDev/smarthome/core/database"
+	"github.com/MikMuellerDev/smarthome/core/event"
+	"github.com/MikMuellerDev/smarthome/core/homescript"
 )
 
 func TestMain(m *testing.M) {
@@ -21,6 +23,12 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		panic(err.Error())
 	}
+	if err := database.CreateRoom("test_room", "", ""); err != nil {
+		panic(err.Error())
+	}
+	if err := database.CreateSwitch("test_switch", "", "test_room", 0); err != nil {
+		panic(err.Error())
+	}
 	if !doesExists {
 		// Create Homescript
 		if err := database.CreateNewHomescript(database.Homescript{
@@ -30,7 +38,7 @@ func TestMain(m *testing.M) {
 			Description:         "A Homescript for testing purposes",
 			QuickActionsEnabled: false,
 			SchedulerEnabled:    false,
-			Code:                "log('automation_trigger', '', 0)",
+			Code:                "log('automation_trigger', '', 0); switch('test_switch', on)",
 		}); err != nil {
 			panic(err.Error())
 		}
@@ -61,6 +69,63 @@ func initDB(args ...bool) error {
 	return nil
 }
 
+func TestAutomation(t *testing.T) {
+	now := time.Now()
+	then := now.Add(time.Minute)
+	log := logrus.New()
+	log.Level = logrus.FatalLevel
+	event.InitLogger(log)
+	homescript.InitLogger(log)
+	// Flush all logs before automation runs
+	if err := database.FlushAllLogs(); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if err := Init(); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if err := CreateNewAutomation(
+		"name",
+		"description",
+		uint8(then.Hour()),
+		uint8(then.Minute()),
+		[]uint8{0, 1, 2, 3, 4, 5, 6},
+		"test",
+		"admin",
+		true,
+		database.TimingNormal,
+	); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	time.Sleep(time.Minute * 2)
+	logs, err := database.GetLogs()
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	valid := false
+	for _, l := range logs {
+		if l.Name == "automation_trigger" && l.Level == 0 {
+			valid = true
+		}
+	}
+	if !valid {
+		t.Error("Log from automation not found, could not verify that automation has been executed")
+		return
+	}
+	power, err := database.GetPowerStateOfSwitch("test_switch")
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if !power {
+		t.Error("Power did not change: want: true got: false")
+		return
+	}
+}
+
 func TestInit(t *testing.T) {
 	if err := Init(); err != nil {
 		t.Error(err.Error())
@@ -68,15 +133,11 @@ func TestInit(t *testing.T) {
 	}
 }
 
-func TestDeactivate(t *testing.T) {
-	TestInit(t) // Initialize the system first
+func TestActivate(t *testing.T) {
+	TestInit(t)
 	if err := DeactivateAutomationSystem(); err != nil {
 		t.Error(err.Error())
 	}
-}
-
-func TestActivate(t *testing.T) {
-	TestInit(t)
 	if err := ActivateAutomationSystem(); err != nil {
 		t.Error(err.Error())
 	}
