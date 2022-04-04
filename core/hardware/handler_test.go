d9ad00f1ff0eac3434cdf44574ec8ba4ca8ed719
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-04 22:05:48 +0200

    Add async power handling test

--- core/hardware/handler_test.go
+++ core/hardware/handler_test.go
@@ -2,7 +2,9 @@ package hardware
 
 import (
 	"strings"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/sirupsen/logrus"
 
@@ -93,5 +95,105 @@ func TestSetPower(t *testing.T) {
 	if err := SetPower("test", false); err != nil {
 		t.Error(err.Error())
 		return
+	}
+}
+
+func TestSetPowerAsync(t *testing.T) {
+	log := logrus.New()
+	log.Level = logrus.FatalLevel
+	InitLogger(log)
+	event.InitLogger(log)
+	if err := initDB(true); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if err := database.CreateHardwareNode(database.HardwareNode{
+		Name:    "test",
+		Online:  true,
+		Enabled: true,
+		Url:     "http://localhost",
+		Token:   "",
+	}); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	table := []struct {
+		Switch string
+		Power  bool
+		Error  string
+	}{
+		{
+			Switch: "test_1",
+			Power:  true,
+			// Only the first request will throw an error due to node being marked as offline
+			Error: `Post "http://localhost/power?token=": dial tcp`, // Different on other machines
+		},
+		{
+			Switch: "test_1",
+			Power:  false,
+			Error:  ``,
+		},
+		{
+			Switch: "test_2",
+			Power:  true,
+			Error:  ``,
+		},
+		{
+			Switch: "test_2",
+			Power:  false,
+			Error:  ``,
+		},
+	}
+	// Create a test room
+	if err := database.CreateRoom("test", "test", "test"); err != nil {
+		t.Error("Failed to create room:", err.Error())
+		return
+	}
+	var wg sync.WaitGroup
+	for _, req := range table {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := database.CreateSwitch(req.Switch, req.Switch, "test", 0); err != nil {
+				t.Error(err.Error())
+				return
+			}
+			if err := SetPower(req.Switch, req.Power); err != nil {
+				if !strings.Contains(err.Error(), req.Error) || req.Error == "" {
+					t.Errorf("Unexpected error: want: `%s` got: `%s`", req.Error, err.Error())
+					return
+				}
+			} else if req.Error != "" {
+				t.Errorf("Expected error: want: `%s` got: `%s`", req.Error, "")
+				return
+			}
+			powerState, err := GetPowerState(req.Switch)
+			if err != nil {
+				t.Error(err.Error())
+				return
+			}
+			time.Sleep(time.Second)
+			if powerState != req.Power {
+				t.Errorf("Power state unaffected: want: `%t` got: `%t`", req.Power, powerState)
+				return
+			}
+			if len(GetPendingJobs()) != GetPendingJobCount() {
+				t.Errorf("Pending job count (%d) does not match count of current jobs (%d)", GetPendingJobCount(), len(GetPendingJobs()))
+				return
+			}
+		}()
+		// When no node is registered, the request should not fail
+		if err := database.DeleteHardwareNode("http://localhost"); err != nil {
+			t.Error(err.Error())
+			return
+		}
+		if err := SetPower("test", false); err != nil {
+			t.Error(err.Error())
+			return
+		}
+		if GetPendingJobCount() != 0 {
+			t.Errorf("Current job count invalid. want: 0 got: %d", GetPendingJobCount())
+			return
+		}
 	}
 }
