abce6fb6fbb345eca938a92f02c2f7877bb5cecd
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-09 21:40:27 +0200

    Update Reminder.svelte

--- web/src/pages/reminders/Reminder.svelte
+++ web/src/pages/reminders/Reminder.svelte
@@ -1,24 +1,46 @@
 <script lang="ts">
-    export let id: number
-    export let name: string
-    export let description: string
-    export let priority: number
-    export let dueDate: string
-    export let createdDate: string
-    export let userWasNotified: boolean
+  import { onMount } from "svelte";
+
+  export let id: number;
+  export let name: string;
+  export let description: string;
+  export let priority: number;
+  export let dueDate: string;
+  export let createdDate: string;
+  export let userWasNotified: boolean;
+
+  let priorityColor: string;
+
+  onMount(() => {
+    switch (priority) {
+      case 0:
+        priorityColor = "#ff0000";
+        break;
+      case 1:
+        priorityColor = "#00ff00";
+        break;
+      case 2:
+        priorityColor = "#0000ff";
+        break;
+      default:
+        priorityColor = "#ff00ff";
+        break;
+    }
+  });
 </script>
 
-<div class="mdc-elevation--z3">
-    <h5>{name}</h5>
-    <p>{description}</p>
-    <p>{dueDate}</p>
-    <p class="text-hint">{createdDate}</p>
+<div id="main" class="mdc-elevation--z3" style:--clr-priority={priorityColor}>
+  <h5>{name}</h5>
+  <p>{description}</p>
+  <p>{dueDate}</p>
+  <p class="text-hint">{createdDate}</p>
 </div>
 
 <style lang="scss">
-    div {
-        background-color: var(--clr-height-1-3);
-        display: flex;
-        border-radius: .3rem;
-    }
+  #main {
+    background-color: var(--clr-height-1-3);
+    display: flex;
+    border-radius: 0.3rem;
+    align-items: center;
+  }
 </style>
