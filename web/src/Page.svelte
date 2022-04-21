e6670007b3735c9f1e42eb6fc1435d1a529a1267
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-21 10:39:32 +0200

    Move `contrast` to `global.ts`

--- web/src/Page.svelte
+++ web/src/Page.svelte
@@ -2,34 +2,32 @@
     import type { KitchenComponentDev } from '@smui/snackbar/kitchen'
     import Kitchen,{ ConfigAction } from '@smui/snackbar/kitchen'
     import NavBar from './components/NavBar.svelte'
-    import { createSnackbar,data } from './global'
+    import { contrast,createSnackbar,data } from './global'
 
-    function contrast(color: string): 'black' | 'white' {
-        const r = parseInt(color.slice(1, 3), 16)
-        const g = parseInt(color.slice(3, 5), 16)
-        const b = parseInt(color.slice(5, 7), 16)
-        const a = [r, g, b].map(v => {
-            v /= 255
-            return v <= 0.03928
-                ? v / 12.92
-                : Math.pow((v + 0.055) / 1.055, 2.4)
-        })
-        const luminance = a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722
-        const [darker, brighter] = [1.05, luminance + 0.05].sort()
-        return brighter / darker <= 4.5 ? 'black' : 'white'
-    }
-
-    $: document.documentElement.classList.toggle('light-theme', !$data.userData.user.darkTheme)
+    $: document.documentElement.classList.toggle(
+        'light-theme',
+        !$data.userData.user.darkTheme
+    )
     $: if ($data.loaded) {
-        document.documentElement.style.setProperty('--clr-primary-dark', $data.userData.user.primaryColorDark)
-        document.documentElement.style.setProperty('--clr-primary-light', $data.userData.user.primaryColorLight)
+        document.documentElement.style.setProperty(
+            '--clr-primary-dark',
+            $data.userData.user.primaryColorDark
+        )
+        document.documentElement.style.setProperty(
+            '--clr-primary-light',
+            $data.userData.user.primaryColorLight
+        )
         document.documentElement.style.setProperty(
             '--clr-on-primary-dark',
-            contrast($data.userData.user.primaryColorDark) === 'black' ? '#121212' : '#ffffff',
+            contrast($data.userData.user.primaryColorDark) === 'black'
+                ? '#121212'
+                : '#ffffff'
         )
         document.documentElement.style.setProperty(
             '--clr-on-primary-light',
-            contrast($data.userData.user.primaryColorLight) === 'black' ? '#121212' : '#ffffff',
+            contrast($data.userData.user.primaryColorLight) === 'black'
+                ? '#121212'
+                : '#ffffff'
         )
     }
 
@@ -45,12 +43,12 @@
 
 <svelte:head>
     {#if !$data.userData.user.darkTheme}
-        <link rel="stylesheet" href="/assets/theme-light.css">
+        <link rel="stylesheet" href="/assets/theme-light.css" />
     {/if}
 </svelte:head>
 <NavBar />
 <main>
-    <slot></slot>
+    <slot />
 </main>
 <Kitchen bind:this={kitchen} dismiss$class="material-icons" />
 
@@ -59,7 +57,7 @@
 
     main {
         margin-left: 5.125rem;
-        transition: margin-left .3s;
+        transition: margin-left 0.3s;
         @include mobile {
             margin-left: 0;
             margin-top: 3.5rem;
--- web/src/global.ts
+++ web/src/global.ts
@@ -30,6 +30,22 @@ export interface UserData {
     permissions: string[]
 }
 
+// Given an arbitrary input color, the function decides whether text on the color should be white or black
+export function contrast(color: string): 'black' | 'white' {
+    const r = parseInt(color.slice(1, 3), 16)
+    const g = parseInt(color.slice(3, 5), 16)
+    const b = parseInt(color.slice(5, 7), 16)
+    const a = [r, g, b].map(v => {
+        v /= 255
+        return v <= 0.03928
+            ? v / 12.92
+            : Math.pow((v + 0.055) / 1.055, 2.4)
+    })
+    const luminance = a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722
+    const [darker, brighter] = [1.05, luminance + 0.05].sort()
+    return brighter / darker <= 4.5 ? 'black' : 'white'
+}
+
 // Color caching
 const cachedColorDark: string = localStorage.getItem("smarthome_primary_color_dark")
 const cachedColorLight: string = localStorage.getItem("smarthome_primary_color_light")
@@ -39,8 +55,22 @@ const cachedThemeTemp = localStorage.getItem("smarthome_theme") === 'true'
 if (cachedColorDark !== null && cachedColorLight !== null && cachedTheme !== null) {
     document.documentElement.style.setProperty('--clr-primary-dark', cachedColorDark)
     document.documentElement.style.setProperty('--clr-primary-light', cachedColorLight)
+
+    document.documentElement.style.setProperty(
+        '--clr-on-primary-dark',
+        contrast(cachedColorDark) === 'black'
+            ? '#121212'
+            : '#ffffff'
+    )
+    document.documentElement.style.setProperty(
+        '--clr-on-primary-light',
+        contrast(cachedColorLight) === 'black'
+            ? '#121212'
+            : '#ffffff'
+    )
+
     document.documentElement.classList.toggle('light-theme', cachedTheme)
-    cachedTheme = cachedThemeTemp 
+    cachedTheme = cachedThemeTemp
 }
 
 export const data: Writable<Data> = writable({
