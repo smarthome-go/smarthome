0399f9cd7dd1715edd30aacc847de6f1a277889c
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-26 19:33:13 +0200

    Add room modification API endpoint

--- server/api/room.go
+++ server/api/room.go
@@ -8,7 +8,7 @@ import (
 	"github.com/MikMuellerDev/smarthome/server/middleware"
 )
 
-type AddRoomRequest struct {
+type RoomRequest struct {
 	Id          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -33,12 +33,11 @@ func GetUserRoomsWithSwitches(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Add a new Room
 func AddRoom(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
-	var request AddRoomRequest
+	var request RoomRequest
 	if err := decoder.Decode(&request); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		Res(w, Response{Success: false, Message: "bad request", Error: "invalid request body"})
@@ -61,4 +60,39 @@ func AddRoom(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	Res(w, Response{Success: true, Message: "successfully created new room"})
+}
+
+// Modifies the room's name and description
+func ModifyRoomData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	var request RoomRequest
+	if err := decoder.Decode(&request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		Res(w, Response{Success: false, Message: "bad request", Error: "invalid request body"})
+		return
+	}
+
+	room, found, err := database.GetRoomDataById(request.Id)
+	if err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		Res(w, Response{Success: false, Message: "failed to modify room", Error: "database failure"})
+		return
+	}
+	if !found {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		Res(w, Response{Success: false, Message: "failed to modify room", Error: "invalid room-id"})
+		return
+	}
+	if room.Name == request.Name && room.Description == request.Description {
+		Res(w, Response{Success: true, Message: "data unchanged"})
+		return
+	}
+	if err := database.ModifyRoomData(request.Id, request.Name, request.Description); err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		Res(w, Response{Success: false, Message: "failed to modify room", Error: "database failure"})
+		return
+	}
+	Res(w, Response{Success: true, Message: "successfully modified room"})
 }
