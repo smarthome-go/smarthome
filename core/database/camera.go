5bee38e4627ca62cbfcec77de2da2528e54a87b4
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-26 19:32:33 +0200

    Add camera database functions with tests

--- core/database/camera.go
+++ core/database/camera.go
@@ -1,23 +1,153 @@
 package database
 
+import "database/sql"
+
+type Camera struct {
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	Url    string `json:"url"`
+	RoomId string `json:"roomId"`
+}
+
 func createCameraTable() error {
-	query := `
+	_, err := db.Exec(`
 	CREATE TABLE
 	IF NOT EXISTS
 	camera(
-		Id INT AUTO_INCREMENT,
-		RoomId VARCHAR(30),
-		Url text,
+		Id VARCHAR(50) PRIMARY KEY,
 		Name VARCHAR(50),
-		PRIMARY KEY(Id),
-		CONSTRAINT CameraRoomId
-		FOREIGN KEY (RoomId)
-		REFERENCES room(Id)
+		Url text,
+		RoomId VARCHAR(30),
+		FOREIGN KEY (RoomId) REFERENCES room(Id)
 	)
-	`
-	_, err := db.Exec(query)
+	`)
 	if err != nil {
 		log.Error("Failed to create camera table: executing query failed: ", err.Error())
+		return err
+	}
+	return nil
+}
+
+func CreateCamera(data Camera) error {
+	query, err := db.Prepare(`
+	INSERT INTO
+	camera(
+		Id,
+		Name,
+		Url,
+		RoomId
+	)
+	VALUES(?, ?, ?, ?)
+	ON DUPLICATE KEY
+		UPDATE
+		Name=VALUES(Name)
+	`)
+	if err != nil {
+		log.Error("Failed to create camera: preparing query failed: ", err.Error())
+		return err
+	}
+	if _, err := query.Exec(
+		data.Id,
+		data.Name,
+		data.Url,
+		data.RoomId,
+	); err != nil {
+		log.Error("Failed to create camera: executing query failed: ", err.Error())
+		return err
+	}
+	return nil
+}
+
+func ListCameras() ([]Camera, error) {
+	res, err := db.Query(`
+	SELECT
+		Id,
+		Name,
+		Url,
+		RoomId
+	FROM camera
+	`)
+	if err != nil {
+		log.Error("Failed to list cameras: executing query failed: ", err.Error())
+		return nil, err
+	}
+
+	cameras := make([]Camera, 0)
+	for res.Next() {
+		var camera Camera
+		if err := res.Scan(
+			&camera.Id,
+			&camera.Name,
+			&camera.Url,
+			&camera.RoomId,
+		); err != nil {
+			log.Error("Failed to list cameras: scanning results failed: ", err.Error())
+			return nil, err
+		}
+		cameras = append(cameras, camera)
+	}
+	return cameras, nil
+}
+
+func GetCameraById(id string) (Camera, bool, error) {
+	query, err := db.Prepare(`
+	SELECT
+		Id,
+		Name,
+		Url,
+		RoomId
+	FROM camera
+	WHERE Id=?
+	`)
+	if err != nil {
+		log.Error("Failed to get camera by id: preparing query failed: ", err.Error())
+		return Camera{}, false, err
+	}
+	var camera Camera
+	if err := query.QueryRow(id).Scan(
+		&camera.Id,
+		&camera.Name,
+		&camera.Url,
+		&camera.RoomId,
+	); err != nil {
+		if err == sql.ErrNoRows {
+			return Camera{}, false, nil
+		}
+		log.Error("Failed to get camera by id: executing query failed: ", err.Error())
+		return Camera{}, false, err
+	}
+	return camera, true, nil
+}
+
+func DeleteCamera(id string) error {
+	query, err := db.Prepare(`
+	DELETE FROM camera
+	WHERE Id=?
+	`)
+	if err != nil {
+		log.Error("Failed to delete camera: preparing query failed: ", err.Error())
+		return err
+	}
+	if _, err := query.Exec(id); err != nil {
+		log.Error("Failed to delete camera: executing query failed: ", err.Error())
+		return err
+
+	}
+	return nil
+}
+
+// Deletes all cameras in an arbitrary room
+func DeleteRoomCameras(roomId string) error {
+	query, err := db.Prepare(`
+	DELETE FROM camera
+	WHERE RoomId=?
+	`)
+	if err != nil {
+		log.Error("Failed to delete room cameras: preparing query failed: ", err.Error())
+		return err
+	}
+	if _, err := query.Exec(roomId); err != nil {
+		log.Error("Failed to delete room cameras: executing query failed: ", err.Error())
 		return err
 	}
 	return nil
--- core/database/camera_test.go
+++ core/database/camera_test.go
@@ -1,9 +1,62 @@
 package database
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCreateCameraTable(t *testing.T) {
 	if err := createCameraTable(); err != nil {
 		t.Error(err.Error())
 	}
+}
+
+func TestCreateCamera(t *testing.T) {
+	// Create test rooms
+	if err := CreateRoom(RoomData{Id: "test"}); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	if err := CreateRoom(RoomData{Id: "test2"}); err != nil {
+		t.Error(err.Error())
+		return
+	}
+
+	table := []Camera{
+		{
+			Id:     "test_1",
+			Name:   "test 1",
+			Url:    "http://example/com/1",
+			RoomId: "test",
+		},
+		{
+			Id:     "test_2",
+			Name:   "test 2",
+			Url:    "http://example.com/2",
+			RoomId: "test2",
+		},
+	}
+
+	for _, camera := range table {
+		if err := CreateCamera(camera); err != nil {
+			t.Error(err.Error())
+		}
+
+		cam, found, err := GetCameraById(camera.Id)
+		if err != nil {
+			t.Error(err.Error())
+		}
+		if !found {
+			t.Errorf("Camera %s not found after creation", camera.Id)
+		}
+		assert.Equal(t, camera, cam)
+	}
+
+	// Check the listing function
+	cameras, err := ListCameras()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	assert.Equal(t, cameras, table)
 }
