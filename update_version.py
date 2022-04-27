05b8239f2d24825d4914c23606f3ef6b33b17ba0
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-04-27 21:54:33 +0200

    Make `update_version.py` compatible with `README.md`

--- update_version.py
+++ update_version.py
@@ -18,7 +18,7 @@ with open(main_go_path, "r") as main_go:
 
 try:
     VERSION = input(
-    f"Current version: {old_version}\nNew version (without 'v' prefix): ")
+        f"Current version: {old_version}\nNew version (without 'v' prefix): ")
 except KeyboardInterrupt:
     print("\nCanceled by user")
     quit()
@@ -43,7 +43,7 @@ with open(docker_motd_path, "r") as docker_motd:
 
 with open(docker_motd_path, "w") as main_go:
     main_go.write(content.replace(old_version, VERSION))
-    
+
 # The Dockerfile
 with open(dockerfile_path, "r") as dockerfile:
     content = dockerfile.read()
@@ -52,7 +52,7 @@ with open(dockerfile_path, "r") as dockerfile:
 
 with open(dockerfile_path, "w") as dockerfile:
     dockerfile.write(content.replace(old_version, VERSION))
-    
+
 # The NPM `package.json`
 with open(package_json_path, "r") as package_json:
     content = package_json.read()
@@ -61,32 +61,33 @@ with open(package_json_path, "r") as package_json:
 
 with open(package_json_path, "w") as package_json:
     package_json.write(content.replace(old_version, VERSION))
-    
+
 # The `README.md`
 with open(readme_path, "r") as readme:
     content = readme.read()
-    old_version = content.split("### Version: `")[1].split("`\n")[0]
+    old_version = content.split("**Version**: `")[1].split("`\n")[0]
     print(f"Found old version in {readme_path}:", old_version)
-    
+
 with open(readme_path, "w") as readme:
     readme.write(content.replace(old_version, VERSION))
-    
+
 # The `Makefile`
 with open(makefile_path, "r") as makefile:
     content = makefile.read()
     old_version = content.split("version := ")[1].split("\n")[0]
     print(f"Found old version in {makefile_path}:", old_version)
-    
+
 with open(makefile_path, "w") as makefile:
     makefile.write(content.replace(old_version, VERSION))
 
 # The `docker_compose.yml`
 with open(docker_compose_path, "r") as compose:
     content = compose.read()
-    old_version = content.split("image: mikmuellerdev/smarthome:")[1].split("\n")[0]
+    old_version = content.split(
+        "image: mikmuellerdev/smarthome:")[1].split("\n")[0]
     print(f"Found old version in {docker_compose_path}:", old_version)
-    
+
 with open(docker_compose_path, "w") as compose:
     compose.write(content.replace(old_version, VERSION))
-    
+
 print(f"Version has been upgraded from '{old_version}' -> '{VERSION}'")
