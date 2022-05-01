43822eea0fb05c3dc218e39b0a767d521682087f
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-05-01 15:17:25 +0200

    Remove monaco editor

--- web/src/pages/editor/App.svelte
+++ web/src/pages/editor/App.svelte
@@ -1,30 +1,3 @@
-  
-  <script>
-    import Monaco from './monaco.svelte'
-    
-    let monaco;
-    window['monaco'] = monaco;
-  </script>
-
-
-<div id="app">
-    <Monaco bind:this={monaco} />
-  </div>
-  
-  <style>
-    #app {
-      width: 100vw;
-      height: 100vh;
-      position: relative;
-      display: grid;
-      justify-content: center;
-      align-content: center;
-      grid-template-columns: 100%;
-      grid-template-rows: 100%;
-    }
-    * :global(.buttons) {
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(200px,1fr));
-      width: 500px;
-    }
-  </style>
+<div>
+	Editor here
+</div>
