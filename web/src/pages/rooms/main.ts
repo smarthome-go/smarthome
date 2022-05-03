37ebc30243a43a8cf788ba68d08175a32ddfbe2f
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-05-03 22:54:18 +0200

    Add camera interface declarations

--- web/src/pages/rooms/main.ts
+++ web/src/pages/rooms/main.ts
@@ -2,25 +2,30 @@ import { writable, Writable } from 'svelte/store'
 import App from './App.svelte'
 
 export interface Room {
-  data: {
-      id: string
-      name: string
-      description: string
-  }
-  switches: SwitchResponse[]
-  cameras: Camera[]
+    data: {
+        id: string
+        name: string
+        description: string
+    }
+    switches: SwitchResponse[]
+    cameras: Camera[]
 }
 export interface SwitchResponse {
-  id: string
-  name: string
-  powerOn: boolean
-  watts: number
+    id: string
+    name: string
+    powerOn: boolean
+    watts: number
 }
 
-export interface Camera {}
+export interface Camera {
+    id: string
+    name: string
+    url: string
+    roomId: string
+}
 
 export const loading: Writable<boolean> = writable(false)
 
 export default new App({
-  target: document.body,
+    target: document.body,
 })
