e578e4f518c8e686dab2ee4edcd6a5f7db814ca8
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-05-30 20:17:20 +0200

    Add first version of ace code editor

---
+++ web/src/components/HmsEditor.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { AceEditor } from "svelte-ace";
+    import "brace/mode/json";
+    import "brace/theme/chrome";
+    import "brace/theme/github";
+    let text = "";
+</script>
+
+<AceEditor
+    on:selectionChange={(obj) => console.log(obj.detail)}
+    on:paste={(obj) => console.log(obj.detail)}
+    on:input={(obj) => console.log(obj.detail)}
+    on:focus={() => console.log("focus")}
+    on:documentChange={(obj) => console.log(`document change : ${obj.detail}`)}
+    on:cut={() => console.log("cut")}
+    on:cursorChange={() => console.log("cursor change")}
+    on:copy={() => console.log("copy")}
+    on:init={(editor) => console.log(editor.detail)}
+    on:commandKey={(obj) => console.log(obj.detail)}
+    on:changeMode={(obj) => console.log(`change mode : ${obj.detail}`)}
+    on:blur={() => console.log("blur")}
+    width="100%"
+    height="300px"
+    lang="json"
+    theme="github"
+    value={text}
+/>
+
