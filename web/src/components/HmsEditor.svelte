cbb316e13611c1dc71d2be84f26a3a1e157d63fe
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-05-30 21:41:12 +0200

    Add codemirror test

--- web/src/components/HmsEditor.svelte
+++ web/src/components/HmsEditor.svelte
@@ -1,28 +1,64 @@
 <script lang="ts">
-    import { AceEditor } from "svelte-ace";
-    import "brace/mode/json";
-    import "brace/theme/chrome";
-    import "brace/theme/github";
-    let text = "";
+    import {
+        EditorState,
+        EditorView,
+        basicSetup,
+    } from "@codemirror/basic-setup";
+    import { EditorSelection, Compartment } from "@codemirror/state";
+    import { javascript } from "@codemirror/lang-javascript";
+    import { onMount } from "svelte";
+
+    let editorDiv: HTMLElement = null;
+
+    let myTheme = EditorView.theme(
+        {
+            "&": {
+                color: "white",
+                backgroundColor: "#034",
+            },
+            ".cm-content": {
+                caretColor: "#0e9",
+            },
+            "&.cm-focused .cm-cursor": {
+                borderLeftColor: "#0e9",
+            },
+            "&.cm-focused .cm-selectionBackground, ::selection": {
+                backgroundColor: "#074",
+            },
+            ".cm-gutters": {
+                backgroundColor: "#045",
+                color: "#ddd",
+                border: "none",
+            },
+        },
+        { dark: true }
+    );
+
+    onMount(() => {
+        let editor = new EditorView({
+            state: EditorState.create({
+                extensions: [basicSetup, javascript(), myTheme],
+            }),
+            parent: editorDiv,
+        });
+
+        editor.dispatch(
+            editor.state.changeByRange((range) => ({
+                changes: [
+                    { from: range.from, insert: "_" },
+                    { from: range.to, insert: "_" },
+                ],
+                range: EditorSelection.range(range.from + 2, range.to + 2),
+            }))
+        );
+    });
 </script>
 
-<AceEditor
-    on:selectionChange={(obj) => console.log(obj.detail)}
-    on:paste={(obj) => console.log(obj.detail)}
-    on:input={(obj) => console.log(obj.detail)}
-    on:focus={() => console.log("focus")}
-    on:documentChange={(obj) => console.log(`document change : ${obj.detail}`)}
-    on:cut={() => console.log("cut")}
-    on:cursorChange={() => console.log("cursor change")}
-    on:copy={() => console.log("copy")}
-    on:init={(editor) => console.log(editor.detail)}
-    on:commandKey={(obj) => console.log(obj.detail)}
-    on:changeMode={(obj) => console.log(`change mode : ${obj.detail}`)}
-    on:blur={() => console.log("blur")}
-    width="100%"
-    height="300px"
-    lang="json"
-    theme="github"
-    value={text}
-/>
+<div id="editor" bind:this={editorDiv} />
 
+<style lang="scss">
+    #editor {
+        width: 100%;
+        height: 100%;
+    }
+</style>
