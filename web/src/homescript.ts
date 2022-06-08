1c73b89ea6e69b6bac2520b9ad78f3a15037d6da
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-06-08 09:19:12 +0200

    Fix typo in type definition in Homescript file

--- web/src/homescript.ts
+++ web/src/homescript.ts
@@ -46,7 +46,7 @@ export interface location {
 
 /* Homescript arguments */
 // Container for homescript argument
-export interface homecriptArg {
+export interface homescriptArg {
     id: number
     data: homescriptArgData
 }
