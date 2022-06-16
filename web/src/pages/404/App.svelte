14f00aca83e0bbc560bc26c946e36e7ae02c7db8
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-06-16 11:07:09 +0200

    Refactor 404 page frontend

--- web/src/pages/404/App.svelte
+++ web/src/pages/404/App.svelte
@@ -1,28 +1,37 @@
 <script lang="ts">
-    import Button, { Label } from '@smui/button'
-    import IconButton from '@smui/icon-button'
+    import Button, { Label } from "@smui/button";
+    import IconButton from "@smui/icon-button";
 
-    let theme = window.localStorage.getItem('theme')
-    if (theme === null) theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-    let darkTheme = theme !== 'light'
-    $: document.documentElement.classList.toggle('light-theme', !darkTheme)
-    $: window.localStorage.setItem('theme', darkTheme ? 'dark' : 'light')
+    // Attempt to retrieve theme from local storage
+    let theme = window.localStorage.getItem("theme");
+    if (theme === null)
+        theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+    let darkTheme = theme !== "light";
+
+    $: document.documentElement.classList.toggle("light-theme", !darkTheme);
+    $: window.localStorage.setItem("theme", darkTheme ? "dark" : "light");
 </script>
 
 <svelte:head>
     {#if !darkTheme}
-        <link rel="stylesheet" href="/assets/theme-light.css">
+        <link rel="stylesheet" href="/assets/theme-light.css" />
     {/if}
 </svelte:head>
+
 <main class="mdc-elevation--z2">
     <IconButton
         id="theme-toggle"
-        on:click={() => darkTheme = !darkTheme}
+        on:click={() => (darkTheme = !darkTheme)}
         class="material-icons"
         title="Toggle light/dark theme"
-    >{darkTheme ? 'light_mode' : 'dark_mode'}</IconButton>
+        >{darkTheme ? "light_mode" : "dark_mode"}</IconButton
+    >
+
     <h1>404</h1>
     <h4>Page not found</h4>
+
     <Button variant="raised" href="/dash">
         <Label>Go back</Label>
     </Button>
@@ -31,15 +40,16 @@
 <style lang="scss">
     main {
         background-color: var(--clr-height-0-2);
-        border-radius: .4rem;
+        border-radius: 0.4rem;
         position: fixed;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
         padding: 3rem;
-        // min-width: 50rem;
 
-        h1 { margin-bottom: 0; }
+        h1 {
+            margin-bottom: 0;
+        }
     }
 
     main :global #theme-toggle {
