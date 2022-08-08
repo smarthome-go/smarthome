ec971cb953beaf4fae668a97afef8963eebdcb5d
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-08 21:07:49 +0200

    Add `schedulerEnabled` to global user data store

--- web/src/global.ts
+++ web/src/global.ts
@@ -33,6 +33,7 @@ export interface UserData {
         surname: string
         primaryColorDark: string
         primaryColorLight: string
+        schedulerEnabled: boolean
         darkTheme: boolean
     }
     permissions: string[]
@@ -43,14 +44,14 @@ export function contrast(color: string): 'black' | 'white' {
     const r = parseInt(color.slice(1, 3), 16)
     const g = parseInt(color.slice(3, 5), 16)
     const b = parseInt(color.slice(5, 7), 16)
-    const a = [ r, g, b ].map(v => {
+    const a = [r, g, b].map(v => {
         v /= 255
         return v <= 0.03928
             ? v / 12.92
             : Math.pow((v + 0.055) / 1.055, 2.4)
     })
-    const luminance = a[ 0 ] * 0.2126 + a[ 1 ] * 0.7152 + a[ 2 ] * 0.0722
-    const [ darker, brighter ] = [ 1.05, luminance + 0.05 ].sort()
+    const luminance = a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722
+    const [darker, brighter] = [1.05, luminance + 0.05].sort()
     return brighter / darker <= 4.5 ? 'black' : 'white'
 }
 
