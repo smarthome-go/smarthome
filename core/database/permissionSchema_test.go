a83b460b4f6e93501bb8e4da7ec7f7d77d1a637b
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-09 00:22:13 +0200

    Fix broken integration test

--- core/database/permissionSchema_test.go
+++ core/database/permissionSchema_test.go
@@ -6,12 +6,12 @@ import (
 
 func TestPermissionsList(t *testing.T) {
 	for _, permission := range Permissions {
-		if len(permission.Name) < 5 {
+		if len(permission.Name) < 4 {
 			t.Errorf("Name ('%s') of permission %s is not long enough, short names are too hard to understand", permission.Permission, permission.Name)
 			return
 		}
-		if len(permission.Description) < 10 {
-			t.Errorf("Description ('%s') of permission %s is not long enough, short descriptions are often hard to understand", permission.Description, permission.Permission)
+		if len(permission.Description) > 80 {
+			t.Errorf("Description ('%s') of permission %s is too long, long descriptions are often hard to display", permission.Description, permission.Permission)
 			return
 		}
 	}
