4617cffbb5b0e61caec40816368dcb65b988903d
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-10 23:39:59 +0200

    Add system page log test

--- web/src/pages/system/App.svelte
+++ web/src/pages/system/App.svelte
@@ -1,7 +1,94 @@
 <script lang="ts">
-    import Page from '../../Page.svelte'
+    import IconButton from "@smui/icon-button";
+
+    import Page from "../../Page.svelte";
+    import Logs from "./logs/Logs.svelte";
+    import Progress from "../../components/Progress.svelte";
 </script>
 
 <Page>
-    System Configuration
-</Page>
+    <div id="header" class="mdc-elevation--z4">
+        <h6>System Configuration</h6>
+        <div id="header__buttons">
+            <IconButton title="Refresh" class="material-icons"
+                >refresh</IconButton
+            >
+        </div>
+    </div>
+    <Progress id="loader" loading={false} />
+    <div id="content">
+        <div id="left" class="mdc-elevation--z1">Some content here</div>
+        <div id="logs" class="mdc-elevation--z1">
+            <Logs />
+        </div>
+    </div></Page
+>
+
+<style lang="scss">
+    @use "../../mixins" as *;
+
+    #header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.1rem 1.3rem;
+        box-sizing: border-box;
+        background-color: var(--clr-height-1-4);
+        min-height: 3.5rem;
+
+        &__buttons {
+            display: flex;
+            align-items: center;
+        }
+
+        h6 {
+            margin: 0.5em 0;
+            @include mobile {
+                // Hide title on mobile due to space limitations
+                display: none;
+            }
+        }
+    }
+
+    #content {
+        padding: 1rem 1.5rem;
+        box-sizing: border-box;
+        flex-direction: column;
+        display: flex;
+        gap: 1rem;
+
+        @include widescreen {
+            height: calc(100vh - 60px);
+            flex-direction: row;
+        }
+
+        @include mobile {
+            min-height: calc(100vh - 48px - 3.5rem);
+            padding: 1rem;
+        }
+
+        #left {
+            background-color: var(--clr-height-0-1);
+            border-radius: 0.4rem;
+            height: 75%;
+            width: 100%;
+
+            @include widescreen {
+                height: 100%;
+                width: 80%;
+            }
+        }
+
+        #logs {
+            background-color: var(--clr-height-0-1);
+            border-radius: 0.4rem;
+            height: 25%;
+            width: 100%;
+
+            @include widescreen {
+                height: 100%;
+                width: 20%;
+            }
+        }
+    }
+</style>
--- web/src/pages/system/main.ts
+++ web/src/pages/system/main.ts
@@ -1,5 +1,30 @@
+import { writable, Writable } from 'svelte/store'
 import App from './App.svelte'
+import "@fontsource/jetbrains-mono";
+
+export interface LogEvent {
+    id: number,
+    name: string,
+    description: string,
+    // TRACE, DEBUG, INFO, WARN, ERROR, FATAL
+    level: 0 | 1 | 2 | 3 | 4 | 5
+    // Time as unix-millis
+    time: number
+}
+
+export const logs: Writable<LogEvent[]> = writable([])
+
+
+
+export const levels = [
+    { label: "TRACE", color: "var(--clr-priority-low)" },
+    { label: "DEBUG", color: "var(--clr-priority-low)" },
+    { label: "INFO", color: "var(--clr-success)" },
+    { label: "WARN", color: "var(--clr-warn) " },
+    { label: "ERROR", color: "var(--clr-error)" },
+    { label: "FATAL", color: "var(--clr-priority-medium)" },
+];
 
 export default new App({
-  target: document.body,
+    target: document.body,
 })
