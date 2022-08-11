fc8afe15e50e2e356f88358d846d678bc6fdd56c
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-11 22:46:39 +0200

    Fix major bugs in database table deletion

--- core/database/database.go
+++ core/database/database.go
@@ -46,34 +46,38 @@ func Shutdown() error {
 
 // Deletes all tables in the active `smarthome` database
 // This function is used in testing and could be used in the future to allow for a system reset
+// Todo: tests this function better
 func DeleteTables() error {
 	// The queries are executed after another and represent raw SQL queries
 	queries := []string{
 		// Required in order to dismiss foreign key constraint errors
 		"SET FOREIGN_KEY_CHECKS = 0",
-		"DROP TABLE IF EXISTS rooms",
-		"DROP TABLE IF EXISTS hasCameraPermission",
-		"DROP TABLE IF EXISTS camera",
-		"DROP TABLE IF EXISTS hasSwitchPermission",
-		"DROP TABLE IF EXISTS switch",
-		"DROP TABLE IF EXISTS schedule",
 		"DROP TABLE IF EXISTS automation",
-		"DROP TABLE IF EXISTS homescript",
-		"DROP TABLE IF EXISTS homescriptArgs",
-		"DROP TABLE IF EXISTS notifications",
-		"DROP TABLE IF EXISTS hasPermission",
-		"DROP TABLE IF EXISTS permission",
-		"DROP TABLE IF EXISTS user",
+		"DROP TABLE IF EXISTS camera",
+		"DROP TABLE IF EXISTS configuration",
 		"DROP TABLE IF EXISTS hardware",
+		"DROP TABLE IF EXISTS hasCameraPermission",
+		"DROP TABLE IF EXISTS hasPermission",
+		"DROP TABLE IF EXISTS hasSwitchPermission",
+		"DROP TABLE IF EXISTS homescript",
+		"DROP TABLE IF EXISTS homescriptArg",
+		"DROP TABLE IF EXISTS homescriptUrlCache",
 		"DROP TABLE IF EXISTS logs",
-		"DROP TABLE IF EXISTS config",
+		"DROP TABLE IF EXISTS notifications",
+		"DROP TABLE IF EXISTS permission",
 		"DROP TABLE IF EXISTS reminder",
+		"DROP TABLE IF EXISTS room",
+		"DROP TABLE IF EXISTS schedule",
+		"DROP TABLE IF EXISTS scheduleSwitches",
+		"DROP TABLE IF EXISTS switch",
+		"DROP TABLE IF EXISTS user",
+		"DROP TABLE IF EXISTS userToken",
 		"SET FOREIGN_KEY_CHECKS = 1",
 	}
 	for _, query := range queries {
 		_, err := db.Exec(query)
 		if err != nil {
-			log.Error(fmt.Sprintf("Failed to execute query %s: %s", query, err.Error()))
+			log.Error(fmt.Sprintf("Failed to execute query `%s`: %s", query, err.Error()))
 			return err
 		}
 	}
