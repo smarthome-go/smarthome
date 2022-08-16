0335d3364f4de3b72b56aa7ba622a67724fddd20
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-16 16:25:54 +0200

    Fix failing weather test

---
+++ core/database/weather.go
@@ -0,0 +1,125 @@
+package database
+
+import (
+	"time"
+)
+
+type WeatherMeasurement struct {
+	Id                 uint      `json:"id"`
+	Time               time.Time `json:"time"`
+	WeatherTitle       string    `json:"weatherTitle"`
+	WeatherDescription string    `json:"weatherDescription"`
+	Temperature        float32   `json:"temperature"`
+	FeelsLike          float32   `json:"feelsLike"`
+	Humidity           uint8     `json:"humidity"`
+}
+
+func createWeatherTable() error {
+	if _, err := db.Exec(`
+	CREATE TABLE
+	IF NOT EXISTS
+	weather(
+		Id						INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		Time					DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+		WeatherTitle			TEXT,
+		WeatherDescription		TEXT,
+		Temperature				FLOAT(24),
+		FeelsLike				FLOAT(24),
+		Humidity				INT UNSIGNED
+	)
+	`); err != nil {
+		log.Error("Failed to create weather data table: executing query failed: ", err.Error())
+		return err
+	}
+	return nil
+}
+
+func GetWeatherDataRecords(maxAgeHours uint) ([]WeatherMeasurement, error) {
+	query, err := db.Prepare(`
+	SELECT
+		Id,
+		Time,
+		WeatherTitle,
+		WeatherDescription,
+		Temperature,
+		FeelsLike,
+		Humidity
+	From weather
+	WHERE
+		Time > NOW() - INTERVAL ? HOUR
+	`)
+	if err != nil {
+		log.Error("Failed to get weather data records: preparing query failed: ", err.Error())
+		return nil, err
+	}
+	defer query.Close()
+	res, err := query.Query(maxAgeHours)
+	if err != nil {
+		log.Error("Failed to get weather data records: executing failed: ", err.Error())
+		return nil, err
+	}
+	results := make([]WeatherMeasurement, 0)
+	for res.Next() {
+		var row WeatherMeasurement
+		// Scan the current row
+		if err := res.Scan(
+			&row.Id,
+			&row.Time,
+			&row.WeatherTitle,
+			&row.WeatherDescription,
+			&row.Temperature,
+			&row.FeelsLike,
+			&row.Humidity,
+		); err != nil {
+			log.Error("Failed to get weather data records: scanning query results failed: ", err.Error())
+			return nil, err
+		}
+		// Append the current row to the results
+		results = append(results, row)
+	}
+	return results, nil
+}
+
+func AddWeatherDataRecord(
+	weatherTitle string,
+	weatherDescription string,
+	temperature float32,
+	feelsLike float32,
+	humidity uint8,
+) (uint, error) {
+	query, err := db.Prepare(`
+	INSERT INTO
+	weather(
+		Id,
+		Time,
+		WeatherTitle,
+		WeatherDescription,
+		Temperature,
+		FeelsLike,
+		Humidity
+	)
+	VALUES(DEFAULT, DEFAULT, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		log.Error("Failed to add weather measurement: preparing query failed: ", err.Error())
+		return 0, err
+	}
+	res, err := query.Exec(
+		weatherTitle,
+		weatherDescription,
+		temperature,
+		feelsLike,
+		humidity,
+	)
+	if err != nil {
+		log.Error("Failed to add weather measurement: executing query failed: ", err.Error())
+		return 0, err
+	}
+	newId, err := res.LastInsertId()
+	if err != nil {
+		log.Error("Failed to add weather measurement: retrieving newly inserted id failed: ", err.Error())
+		return 0, err
+	}
+	return uint(newId), nil
+}
---
+++ core/database/weather_test.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateWeatherTable(t *testing.T) {
+	assert.NoError(t, createWeatherTable())
+}
+
+func TestWeatherData(t *testing.T) {
+	// Add a dummy weather record
+	id, err := AddWeatherDataRecord(
+		"cloudy",
+		"some clouds",
+		42.1,
+		3.1415926,
+		42,
+	)
+	assert.NoError(t, err)
+
+	// Wait 5 seconds to make the new record appear in the query
+	time.Sleep(time.Second * 5)
+
+	// Search the records for the new weather record
+	records, err := GetWeatherDataRecords(1)
+	assert.NoError(t, err)
+	found := false
+	for _, row := range records {
+		if row.Id == id {
+			found = true
+			if row.WeatherTitle != "cloudy" || row.WeatherDescription != "some clouds" || row.Temperature != 42.1 || row.FeelsLike != 3.1415925 || row.Humidity != 42 {
+				t.Error("Invalid data detected when ID matched")
+			}
+		}
+	}
+	assert.True(t, found)
+}
