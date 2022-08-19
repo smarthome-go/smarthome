597a297e0c88bcdee827156d77a5d0eb3108a73d
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-19 14:29:35 +0200

    Fix misleading edit btn. behavior of reminder-edit

--- web/src/pages/reminders/Edit.svelte
+++ web/src/pages/reminders/Edit.svelte
@@ -1,61 +1,72 @@
 <script lang="ts">
-    import Button,{ Label } from '@smui/button'
-    import Dialog,{ Actions,Content,Header,InitialFocus,Title } from '@smui/dialog'
-    import IconButton from '@smui/icon-button'
-    import Inputs from './Inputs.svelte'
+    import Button, { Label } from "@smui/button";
+    import Dialog, {
+        Actions,
+        Content,
+        Header,
+        InitialFocus,
+        Title,
+    } from "@smui/dialog";
+    import IconButton from "@smui/icon-button";
+    import Inputs from "./Inputs.svelte";
 
-    export let modify: (_name: string, _description: string, _priority: number, _dueDate: Date) => Promise<void>
+    export let modify: (
+        _name: string,
+        _description: string,
+        _priority: number,
+        _dueDate: Date
+    ) => Promise<void>;
 
-    let open = false
+    let open = false;
 
-    const priorities = ['Low', 'Normal', 'Medium', 'High', 'Urgent']
+    const priorities = ["Low", "Normal", "Medium", "High", "Urgent"];
 
-    export let inputName = ''
-    export let inputDescription = ''
-    export let inputDueDate: Date
-    export let selectedPriority = 0
-    let priority = priorities[selectedPriority]
+    export let inputName = "";
+    export let inputDescription = "";
+    export let inputDueDate: Date;
+    export let selectedPriority = 0;
+    let priority = priorities[selectedPriority];
 
-    $: selectedPriority = priorities.indexOf(priority)
+    $: selectedPriority = priorities.indexOf(priority);
 
     // Values when the dialog is opened, used for reverting the changes on cancel
-    let nameBefore: string
-    let descriptionBefore: string
-    let dueDateBefore: Date
-    let priorityBefore: number
+    let nameBefore: string;
+    let descriptionBefore: string;
+    let dueDateBefore: Date;
+    let priorityBefore: number;
 
     // Date Picker validation
-    const now = new Date()
-    const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000
-    let datePickerInvalid = false
+    const now = new Date();
+    const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000;
+    let datePickerInvalid = false;
     $: {
         if (inputDueDate !== undefined && inputDueDate !== null)
             datePickerInvalid =
                 now.getTime() - inputDueDate.getTime() + 86400000 >=
-                thirtyDaysInMs // The `8.64e+7` is for adding one extra day
+                thirtyDaysInMs; // The `8.64e+7` is for adding one extra day
     }
 
     function closeHandler(e: CustomEvent<{ action: string }>) {
         switch (e.detail.action) {
-            case 'modify':
+            case "modify":
                 modify(
                     inputName,
                     inputDescription,
                     selectedPriority,
                     inputDueDate
-                )
-                nameBefore = inputName
-                descriptionBefore = inputDescription
-                priorityBefore = selectedPriority
-                dueDateBefore = inputDueDate
-                break
+                );
+                nameBefore = inputName;
+                descriptionBefore = inputDescription;
+                priorityBefore = selectedPriority;
+                dueDateBefore = inputDueDate;
+                break;
             default:
                 // Reset all values to their original state
-                inputName = nameBefore
-                inputDescription = descriptionBefore
-                selectedPriority = priorityBefore
-                inputDueDate = dueDateBefore
-                break
+                inputName = nameBefore;
+                inputDescription = descriptionBefore;
+                selectedPriority = priorityBefore;
+                inputDueDate = dueDateBefore;
+                break;
         }
     }
 </script>
@@ -85,7 +96,15 @@
         <Button defaultAction action="cancel">
             <Label>Cancel</Label>
         </Button>
-        <Button use={[InitialFocus]} disabled={datePickerInvalid} action="modify">
+        <Button
+            use={[InitialFocus]}
+            disabled={datePickerInvalid ||
+                (inputName === nameBefore &&
+                    descriptionBefore === inputDescription &&
+                    priorityBefore === selectedPriority &&
+                    dueDateBefore === inputDueDate)}
+            action="modify"
+        >
             <Label>Modify</Label>
         </Button>
     </Actions>
@@ -94,11 +113,11 @@
 <IconButton
     class="material-icons"
     on:click={async () => {
-        open = true
-        nameBefore = inputName
-        descriptionBefore = inputDescription
-        priorityBefore = selectedPriority
-        dueDateBefore = inputDueDate
+        open = true;
+        nameBefore = inputName;
+        descriptionBefore = inputDescription;
+        priorityBefore = selectedPriority;
+        dueDateBefore = inputDueDate;
     }}
     title="Edit Reminder">edit</IconButton
 >
