16c558c54832d4e51280d1cfde9f654449e0cc87
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2024-03-11 02:49:12 +0100

    chore: Start impl of HMS action dispatcher

--- core/homescript/dispatcher/dispatcher.go
+++ core/homescript/dispatcher/dispatcher.go
@@ -4,10 +4,26 @@ type RegisterInfoMetadata struct {
 }
 
 type RegisterInfo struct {
-	ProgramId string
+	ProgramID string
 	Metadata  RegisterInfoMetadata
 }
 
 type Dispatcher interface {
 	Register(RegisterInfo) error
 }
+
+//
+// Manager interface
+//
+
+type DispatchManager interface {
+	Dispatchers() []Dispatcher
+}
+
+//
+// Manager implementation
+//
+
+// type DispatchManager struct {
+// 	Dispatchers []Dispatcher
+// }
