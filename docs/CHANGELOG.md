03ac9c3d6f300203481e4bf7f6f0fa4fb1dfe174
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2026-07-19 23:36:28 +0200

    feat: Bump version to 0.15.2-alpha

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,4 +1,4 @@
-## Changelog for v0.15.1-alpha
+## Changelog for v0.15.2-alpha
 
 ### META
 
--- main.go
+++ main.go
@@ -29,7 +29,7 @@ const (
 
 func main() {
 	// Do not change the version manually, use the `make version` command instead
-	utils.Version = "0.15.1-alpha"
+	utils.Version = "0.15.2-alpha"
 
 	initLoggers()
 
